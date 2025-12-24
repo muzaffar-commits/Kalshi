86f4be9b3b6b3fe5b722a2a4704fb7f5c92786f2
Muzaffar <mh.dunitech@gmail.com>
2025-12-24 11:23:05 +0530

    text

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/demo/2",
-  assetPrefix: "/demo/2",
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
-
-
