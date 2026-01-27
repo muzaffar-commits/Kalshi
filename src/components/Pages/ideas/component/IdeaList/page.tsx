37d64dc5b2357cf255134991cd2266b6b8763128
visitramsp <vabhi7029@gmail.com>
2026-01-27 10:40:29 +0530

    modify the idea tab comment page

--- src/components/Pages/ideas/component/IdeaList/page.tsx
+++ src/components/Pages/ideas/component/IdeaList/page.tsx
@@ -118,8 +118,8 @@ export default function MobileMenu({
           <Link
             href="#"
             onClick={() => setOpen(false)}
-            className="block text-center bg-[#c8aa76] hover:bg-[#c8aa76]/80
-            py-2 px-4 text-black rounded font-semibold"
+            className="block text-center bg-blue-500 hover:bg-blue-500/80
+            py-2 px-4 text-white rounded font-semibold"
           >
             Post
           </Link>
@@ -141,7 +141,7 @@ function MenuItem({ icon, label, active, onClick }: MenuItemProps) {
     <li onClick={onClick} className="my-4 cursor-pointer text-sm select-none">
       <span
         className={`flex items-center gap-3 transition-colors
-        ${active ? "text-[#c8aa76]" : "text-gray-400 hover:text-[#c8aa76]"}`}
+        ${active ? "dark:text-white text-black font-semibold" : "text-gray-500 dark:hover:text-gray-400 hover:text-gray-400"}`}
       >
         <span className="text-base">{icon}</span>
         {label}
